f8b61abf77b7eb40084259e08dcb6a6167ca3041
Rob <e2robby@gmail.com>
2018-11-22 14:37:59 -0800

    add files for ravencoin status page

--- barMate/app.rb
+++ barMate/app.rb
@@ -1,5 +1,16 @@
 require('sinatra')
+require('./lib/BitcoinRPC')
 
 get ('/') do
   "Docker container and sinatra are set up!"
+end
+
+get ('/status') do
+  query = BitcoinRPC.new('http://brucer:tenderTesticles@172.19.0.2:8766')
+  @rvn_network_info = query.getnetworkinfo
+  @rvn_wallet_info = query.getbalance
+  @rvn_blockchain_info = query.getblockchaininfo
+  @rvn_mining_info =  query.getmininginfo
+  @rvn_mempool_info = query.getmempoolinfo
+  erb(:status)
 end
