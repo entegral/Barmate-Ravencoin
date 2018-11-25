319ced19eafa53c705c6c4fbbcdbf82064e604f6
Rob <e2robby@gmail.com>
2018-11-25 12:14:07 -0800

    update / simplify status page user interface

--- barMate/app.rb
+++ barMate/app.rb
@@ -6,23 +6,33 @@ get ('/') do
 end
 
 get ('/status') do
-  query = BitcoinRPC.new('http://user1:titties1@172.19.0.2:8766')
+  query = BitcoinRPC.new('http://brucer:tenderTesticles@172.19.0.2:8766')
   @rvn_network_info = query.getnetworkinfo
   @rvn_wallet_info = query.getbalance
   @rvn_blockchain_info = query.getblockchaininfo
   @rvn_mining_info =  query.getmininginfo
   @rvn_mempool_info = query.getmempoolinfo
-  @new_rvn_address = query.getnewaddress
-  @list_my_assets = query.listmyassets
+  # @new_rvn_address = query.getnewaddress
+  if @rvn_mining_info['blocks'].to_i > 435456
+    @list_my_assets = query.listmyassets
+  end
   @get_account_addresses = query.getaddressesbyaccount("")
   erb(:status)
 end
 
-post ('/sendAsset') do
-  query = BitcoinRPC.new('http://user1:titties1@172.19.0.2:8766')
+post ('/sendRaven') do
+  query = BitcoinRPC.new('http://brucer:tenderTesticles@172.19.0.2:8766')
   receipient = params.fetch('receipient')
-  ammount = params.fetch('ammount').to_f
+  ammount = params.fetch('ammount')
+  query.sendtoaddress(receipient,ammount.to_i)
+  "Congrats, #{ammount} Raven was sent to #{receipient}"
+end
+
+post ('/sendAsset') do
+  query = BitcoinRPC.new('http://brucer:tenderTesticles@172.19.0.2:8766')
+  receipient = params.fetch('receipient')
+  ammount = params.fetch('ammount')
   asset_name = params.fetch('assetName')
-  query.sendassettoaddress(receipient, asset_name, ammount)
+  query.transfer(asset_name, ammount.to_f, receipient)
   "Congrats, #{ammount} #{asset_name} was sent to #{receipient}"
 end
