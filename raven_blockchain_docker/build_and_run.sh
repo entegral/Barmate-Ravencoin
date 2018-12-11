931f4cba3476d0aa131e8cb6b0cec58243e9230f
Rob <e2robby@gmail.com>
2018-12-10 20:57:24 -0800

    optimize dockerfiles and build scripts

--- raven_blockchain_docker/build_and_run.sh
+++ raven_blockchain_docker/build_and_run.sh
@@ -2,6 +2,7 @@ docker stop ravencoin
 docker rm ravencoin
 docker build -t ravencoin:current  .
 docker run -d \
+    --restart always \
     --network raven-net \
     --ip 172.19.0.2 \
     --name ravencoin \
